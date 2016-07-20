a820fea94d3942fb2d3648b7c10900e7e1b4e73f
LiuQiJiaLinYi <liouqijia@126.com>
2016-07-20 20:35:17 +0800

    Initial Commit

---
+++ LQJPersonalLesson/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LQJPersonalLesson
+//
+//  Created by LiuQiJia on 16/7/20.
+//  Copyright © 2016年 LiuQiJia. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ LQJPersonalLesson/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  LQJPersonalLesson
+//
+//  Created by LiuQiJia on 16/7/20.
+//  Copyright © 2016年 LiuQiJia. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
